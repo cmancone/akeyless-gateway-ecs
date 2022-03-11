b0a535949096adbbf632d9e40b69ed2dcc7f9ab9
Conor Mancone <cmancone@gmail.com>
2022-03-10 22:17:45 -0500

    Forgot to point DNS

--- lb.tf
+++ lb.tf
@@ -46,3 +46,19 @@ resource "aws_security_group_rule" "lb_allow_all_outbound" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.lb.id
 }
+
+data "aws_route53_zone" "gateway" {
+  name = var.route_53_hosted_zone_name
+}
+
+resource "aws_route53_record" "gateway" {
+  zone_id = data.aws_route53_zone.gateway.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.gateway.dns_name
+    zone_id                = aws_lb.gateway.zone_id
+    evaluate_target_health = true
+  }
+}
